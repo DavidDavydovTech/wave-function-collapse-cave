d25fb47735d542f9e2ac5593ffcbf4706a65fb3e
David J. Davydov <davydovjd611@gmail.com>
2020-10-27 13:10:27 -0400

    Add definiations for air and grass blocks

--- src/index.js
+++ src/index.js
@@ -37,11 +37,103 @@ app.loader
 
 // Do stuff when everything is loaded.
 app.loader.load((loader, resources) => {
+    let states = {
+        air: {
+            texture: new PIXI.Texture(resources.tilesheet.texture, new PIXI.Rectangle(0,0,8,8)),
+            autoAccept: true,
+            rules: {
+                top: {},
+                left: {},
+                right: {},
+                bottom: {},
+            }
+        },
+        grassTop: {
+            texture: new PIXI.Texture(resources.tilesheet.texture, new PIXI.Rectangle(1,1,8,8)),
+            autoAccept: false,
+            rules: {
+                top: {},
+                left: {},
+                right: {},
+                bottom: {},
+            }
+        },
+        grassTopLeft: {
+            texture: new PIXI.Texture(resources.tilesheet.texture, new PIXI.Rectangle(0,8,8,8)),
+            autoAccept: false,
+            rules: {
+                top: {},
+                left: {},
+                right: {},
+                bottom: {},
+            }
+        },
+        grassTopRight: {
+            texture: new PIXI.Texture(resources.tilesheet.texture, new PIXI.Rectangle(0,8,8,8), undefined, undefined, 12),
+            autoAccept: false,
+            rules: {
+                top: {},
+                left: {},
+                right: {},
+                bottom: {},
+            }
+        },
+        grassLeft: {
+            texture: new PIXI.Texture(resources.tilesheet.texture, new PIXI.Rectangle(0,16,8,8)),
+            autoAccept: false,
+            rules: {
+                top: {},
+                left: {},
+                right: {},
+                bottom: {},
+            }
+        },
+        grassRight: {
+            texture: new PIXI.Texture(resources.tilesheet.texture, new PIXI.Rectangle(0,16,8,8), undefined, undefined, 12),
+            autoAccept: false,
+            rules: {
+                top: {},
+                left: {},
+                right: {},
+                bottom: {},
+            }
+        },
+        grassBottom: {
+            texture: new PIXI.Texture(resources.tilesheet.texture, new PIXI.Rectangle(8,24,8,8)),
+            autoAccept: false,
+            rules: {
+                top: {},
+                left: {},
+                right: {},
+                bottom: {},
+            }
+        },
+        grassBottomLeft: {
+            texture: new PIXI.Texture(resources.tilesheet.texture, new PIXI.Rectangle(0,24,8,8)),
+            autoAccept: false,
+            rules: {
+                top: {},
+                left: {},
+                right: {},
+                bottom: {},
+            }
+        },
+        grassBottomRight: {
+            texture: new PIXI.Texture(resources.tilesheet.texture, new PIXI.Rectangle(0,24,8,8), undefined, undefined, 12),
+            autoAccept: false,
+            rules: {
+                top: {},
+                left: {},
+                right: {},
+                bottom: {},
+            }
+        },
+    }
     let tiles = PIXI.Sprite.from(resources.tilesheet.texture);
     tiles.scale.x = 4
     tiles.scale.y = 4
-    // tiles.x = 128;
-    // tiles.y = 128;
+    tiles.x = 128;
+    tiles.y = 128;
     app.stage.addChild(tiles);
 });
 
