0a66fdb1ffbd96a0a63175ba837e460444093b95
David J. Davydov <davydovjd611@gmail.com>
2020-10-27 11:36:28 -0400

    Add PUXI to the dependancies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // import {Application, Sprite, BaseTexture, SCALE_MODES} from 'pixi.js';
 import * as PIXI from 'pixi.js';
+import 'puxi.js';
+const uxStage = new PUXI.Stage({
+    width: 512,
+    height: 512
+  });
+
 const SCREEN_WIDTH = window.innerWidth;
 const SCREEN_HEIGHT = window.innerHeight;
 // let x = SCREEN_WIDTH / 4;
@@ -22,6 +28,18 @@ PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST; // Forces pixel art to stay
 // Load assets
 app.loader
     .add('tilesheet', 'img/cavesofgallet_tiles.png'); // Main tilesheet.
+
+// Tile Rules:
+
+// Tile Class:
+// class QuantumTile extends PIXI.Sprite {
+//     constructor(texture, tileOptions) {
+//         super(texture);
+
+        
+//     }
+// }
+
 // Do stuff when everything is loaded.
 app.loader.load((loader, resources) => {
     let tiles = PIXI.Sprite.from(resources.tilesheet.texture);
